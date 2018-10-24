526f959ccd3e5375d645f6acab54ccf67143a22a
Dawood Alnajjar <dawood.alnajjar@lnls.br>
2018-10-24 14:20:03 +0000

    Fixed soft links in install.sh script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-sudo cp build/libCrioLinux.so.0* /usr/local/lib/.
+sudo cp build/libCrioLinux.so.0.1.0 /usr/local/lib/.
+sudo ln -s /usr/local/lib/libCrioLinux.so.0.1.0 /usr/local/lib/libCrioLinux.so.0
+sudo ln -s /usr/local/lib/libCrioLinux.so.0 /usr/local/lib/libCrioLinux.so
+
 sudo cp include/CrioLinux.h /usr/local/include/.
