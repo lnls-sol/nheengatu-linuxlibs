b20a8d2e01f8d6cfd2c6fb7116931051a5504a0c
Dawood <d.najjar@gmail.com>
2018-11-08 16:34:34 -0200

    Adding scaler bit stream. Used for testing.

---
+++ bitfiles/NiFpga_scaler.h
@@ -0,0 +1,71 @@
+/*
+ * Generated with the FPGA Interface C API Generator 17.0.0
+ * for NI-RIO 17.0.0 or later.
+ */
+
+#ifndef __NiFpga_scaler_h__
+#define __NiFpga_scaler_h__
+
+#ifndef NiFpga_Version
+   #define NiFpga_Version 1700
+#endif
+
+#include "NiFpga.h"
+
+/**
+ * The filename of the FPGA bitfile.
+ *
+ * This is a #define to allow for string literal concatenation. For example:
+ *
+ *    static const char* const Bitfile = "C:\\" NiFpga_scaler_Bitfile;
+ */
+#define NiFpga_scaler_Bitfile "NiFpga_scaler.lvbitx"
+
+/**
+ * The signature of the FPGA bitfile.
+ */
+static const char* const NiFpga_scaler_Signature = "1B10C28F7E5F0FDB9E9D0D544BD2FC68";
+
+typedef enum
+{
+   NiFpga_scaler_IndicatorBool_Mod2DIO0 = 0x18002,
+   NiFpga_scaler_IndicatorBool_Scaler0_Done = 0x18012,
+} NiFpga_scaler_IndicatorBool;
+
+typedef enum
+{
+   NiFpga_scaler_ControlBool_Scaler0_Enable = 0x18016,
+   NiFpga_scaler_ControlBool_Scaler0_OneShot = 0x1801A,
+} NiFpga_scaler_ControlBool;
+
+typedef enum
+{
+   NiFpga_scaler_IndicatorArrayU32_Scaler0_Counters = 0x18008,
+} NiFpga_scaler_IndicatorArrayU32;
+
+typedef enum
+{
+   NiFpga_scaler_IndicatorArrayU32Size_Scaler0_Counters = 64,
+} NiFpga_scaler_IndicatorArrayU32Size;
+
+typedef enum
+{
+   NiFpga_scaler_ControlArrayBool_Scaler0_Gate = 0x18004,
+} NiFpga_scaler_ControlArrayBool;
+
+typedef enum
+{
+   NiFpga_scaler_ControlArrayBoolSize_Scaler0_Gate = 64,
+} NiFpga_scaler_ControlArrayBoolSize;
+
+typedef enum
+{
+   NiFpga_scaler_ControlArrayU32_Scaler0_PresetValues = 0x1800C,
+} NiFpga_scaler_ControlArrayU32;
+
+typedef enum
+{
+   NiFpga_scaler_ControlArrayU32Size_Scaler0_PresetValues = 64,
+} NiFpga_scaler_ControlArrayU32Size;
+
+#endif
