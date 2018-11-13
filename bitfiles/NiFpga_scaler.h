a68afcf01bb91f976e2f8f3a46f1f948933492a6
Dawood Alnajjar <dawood.alnajjar@lnls.br>
2018-11-13 15:00:28 +0000

    Adding pulse scaler bitstream

--- bitfiles/NiFpga_scaler.h
+++ bitfiles/NiFpga_scaler.h
@@ -3,8 +3,8 @@
  * for NI-RIO 17.0.0 or later.
  */
 
-#ifndef __NiFpga_scaler_h__
-#define __NiFpga_scaler_h__
+#ifndef __NiFpga_scaler64_example_h__
+#define __NiFpga_scaler64_example_h__
 
 #ifndef NiFpga_Version
    #define NiFpga_Version 1700
@@ -17,55 +17,59 @@
  *
  * This is a #define to allow for string literal concatenation. For example:
  *
- *    static const char* const Bitfile = "C:\\" NiFpga_scaler_Bitfile;
+ *    static const char* const Bitfile = "C:\\" NiFpga_scaler64_example_Bitfile;
  */
-#define NiFpga_scaler_Bitfile "NiFpga_scaler.lvbitx"
+#define NiFpga_scaler64_example_Bitfile "NiFpga_scaler64_example.lvbitx"
 
 /**
  * The signature of the FPGA bitfile.
  */
-static const char* const NiFpga_scaler_Signature = "474E64C23E3B176104E6C19046765F75";
+static const char* const NiFpga_scaler64_example_Signature = "FA92C3A36F91F3D6C1873A65C040D594";
 
 typedef enum
 {
-   NiFpga_scaler_IndicatorBool_Mod2DIO0 = 0x18002,
-   NiFpga_scaler_IndicatorBool_Scaler0_Done = 0x18012,
-} NiFpga_scaler_IndicatorBool;
+   NiFpga_scaler64_example_IndicatorBool_Done_Scaler = 0x1800E,
+} NiFpga_scaler64_example_IndicatorBool;
 
 typedef enum
 {
-   NiFpga_scaler_ControlBool_Scaler0_Enable = 0x18016,
-   NiFpga_scaler_ControlBool_Scaler0_OneShot = 0x1801A,
-} NiFpga_scaler_ControlBool;
+   NiFpga_scaler64_example_IndicatorU64_Pulse_Scaler = 0x18000,
+} NiFpga_scaler64_example_IndicatorU64;
 
 typedef enum
 {
-   NiFpga_scaler_IndicatorArrayU32_Scaler0_Counters = 0x18008,
-} NiFpga_scaler_IndicatorArrayU32;
+   NiFpga_scaler64_example_ControlBool_Enable_Scaler = 0x1801A,
+   NiFpga_scaler64_example_ControlBool_OneShot_Scaler = 0x18006,
+} NiFpga_scaler64_example_ControlBool;
 
 typedef enum
 {
-   NiFpga_scaler_IndicatorArrayU32Size_Scaler0_Counters = 64,
-} NiFpga_scaler_IndicatorArrayU32Size;
+   NiFpga_scaler64_example_IndicatorArrayU32_Counter_Scaler = 0x18008,
+} NiFpga_scaler64_example_IndicatorArrayU32;
 
 typedef enum
 {
-   NiFpga_scaler_ControlArrayBool_Scaler0_Gate = 0x18004,
-} NiFpga_scaler_ControlArrayBool;
+   NiFpga_scaler64_example_IndicatorArrayU32Size_Counter_Scaler = 64,
+} NiFpga_scaler64_example_IndicatorArrayU32Size;
 
 typedef enum
 {
-   NiFpga_scaler_ControlArrayBoolSize_Scaler0_Gate = 64,
-} NiFpga_scaler_ControlArrayBoolSize;
+   NiFpga_scaler64_example_ControlArrayBool_Gate_Scaler = 0x18014,
+} NiFpga_scaler64_example_ControlArrayBool;
 
 typedef enum
 {
-   NiFpga_scaler_ControlArrayU32_Scaler0_PresetValues = 0x1800C,
-} NiFpga_scaler_ControlArrayU32;
+   NiFpga_scaler64_example_ControlArrayBoolSize_Gate_Scaler = 64,
+} NiFpga_scaler64_example_ControlArrayBoolSize;
 
 typedef enum
 {
-   NiFpga_scaler_ControlArrayU32Size_Scaler0_PresetValues = 64,
-} NiFpga_scaler_ControlArrayU32Size;
+   NiFpga_scaler64_example_ControlArrayU32_PresetValues_Scaler = 0x18010,
+} NiFpga_scaler64_example_ControlArrayU32;
+
+typedef enum
+{
+   NiFpga_scaler64_example_ControlArrayU32Size_PresetValues_Scaler = 64,
+} NiFpga_scaler64_example_ControlArrayU32Size;
 
 #endif
