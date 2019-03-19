9a4930fe651c7e2ed5d47e004bddce975fb6054e
Dawood Alnajjar <dawood.alnajjar@lnls.br>
2019-03-19 13:56:54 +0000

    Fixed paths of symbolic links

--- crioInstall.sh
+++ crioInstall.sh
@@ -7,8 +7,8 @@ fi
 
 
 cp -f build/libCrioLinux.so.0.1.0 /usr/local/lib/.
-ln -sf /usr/local/lib/libCrioLinux.so.0.1.0 /usr/local/lib/libCrioLinux.so.0
-ln -sf /usr/local/lib/libCrioLinux.so.0 /usr/local/lib/libCrioLinux.so
+ln -sf libCrioLinux.so.0.1.0 /usr/local/lib/libCrioLinux.so.0
+ln -sf libCrioLinux.so.0 /usr/local/lib/libCrioLinux.so
 
 cp -f include/CrioLinux.h /usr/local/include/.
 ls -alh /usr/local/include/CrioLinux.h /usr/local/lib/libCrioLinux.so*
